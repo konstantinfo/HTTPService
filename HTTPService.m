4a3476131d7a6a0062b613505574f3f07f26b563
Ashish Sharma <kiplmobile02@konstantinfosolutions.com>
2015-05-30 16:18:54 +0530

    First import

---
+++ HTTPService.m
@@ -0,0 +1,254 @@
+//
+//  HTTPService.m
+//
+//  Created by Ashish Sharma on 30/05/15.
+//  Copyright (c) 2015 Konstant Info. All rights reserved.
+//
+
+#import "HTTPService.h"
+
+@interface HTTPService ()
+
+/**
+ *  Call this to set HTTP headers for request
+ *
+ *  @param headers HTTP headers
+ */
+- (void) setHeaders:(NSDictionary*) headers;
+
+/**
+ *  Call this to convert params in raw json format
+ *
+ *  @param params params to post
+ */
+- (void) addQueryStringWithParams:(NSDictionary*) params;
+
+@end
+
+@implementation HTTPService
+
+#pragma mark - Designated Initializer
+
+- (id)initWithBaseURL:(NSURL*) baseURL andSessionConfig:(NSURLSessionConfiguration*) config
+{
+    self = [super init];
+    
+    if (self)
+    {
+        NSAssert(baseURL, ([NSString stringWithFormat:@"\n<------------------->\n%@\n%sn<------------------->",@"baseURL can't be nil",__PRETTY_FUNCTION__]));
+        
+        httpBaseURL = [baseURL absoluteString];
+        
+        if (config)
+            httpSessionManager = [[AFHTTPSessionManager alloc]
+                                  initWithBaseURL:baseURL sessionConfiguration:config];
+        else
+            httpSessionManager = [[AFHTTPSessionManager alloc]
+                                  initWithBaseURL:baseURL sessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+        
+        httpSessionManager.requestSerializer = [AFHTTPRequestSerializer serializer];
+        httpSessionManager.responseSerializer = [AFJSONResponseSerializer serializer];
+    }
+    
+    return self;
+}
+
+#pragma mark - Super Class Methods
+
+- (id)init
+{
+    self = [super init];
+    
+    if (self)
+    {
+        httpBaseURL = BASE_URL;
+        
+        httpSessionManager = [[AFHTTPSessionManager alloc]
+                              initWithBaseURL:[NSURL URLWithString:httpBaseURL]];
+        
+        httpSessionManager.requestSerializer = [AFHTTPRequestSerializer serializer];
+        httpSessionManager.responseSerializer = [AFJSONResponseSerializer serializer];
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    httpSessionManager = nil;
+}
+
+#pragma mark - Instance Methods (Private)
+
+- (void) setHeaders:(NSDictionary*) headers
+{
+    if (headers != nil)
+    {
+        NSArray *allHeaders = [headers allKeys];
+        
+        for (NSString *key in allHeaders)
+        {
+            [httpSessionManager.requestSerializer setValue:[headers objectForKey:key] forHTTPHeaderField:key];
+        }
+    }
+}
+
+- (void) addQueryStringWithParams:(NSDictionary*) params
+{
+    [httpSessionManager.requestSerializer setQueryStringSerializationWithBlock:^NSString *(NSURLRequest *request, NSDictionary *parameters, NSError *__autoreleasing *error) {
+        __block NSMutableString *query = [NSMutableString stringWithString:@""];
+        
+        NSError *err;
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:params options:0 error:&err];
+        NSMutableString *jsonString = [[NSMutableString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        
+        query = jsonString;
+        
+        return query;
+    }];
+}
+
+#pragma mark - Instance Methods
+
+- (void) startRequestWithHttpMethod:(kHttpMethodType) httpMethodType
+                    withHttpHeaders:(NSMutableDictionary*) headers
+                    withServiceName:(NSString*) serviceName
+                     withParameters:(NSMutableDictionary*) params
+                        withSuccess:(void (^)(NSURLSessionDataTask *task, id responseObject))success
+                        withFailure:(void (^)(NSURLSessionDataTask *task, NSError *error))failure
+{
+    NSAssert(serviceName, ([NSString stringWithFormat:@"\n<------------------->\n%@\n%sn<------------------->",@"serviceName can't be nil",__PRETTY_FUNCTION__]));
+    
+    NSString *serviceUrl = [httpBaseURL stringByAppendingPathComponent:serviceName];
+    
+    if (headers == nil)
+    {
+        NSDictionary *headers = [[NSDictionary alloc] initWithObjectsAndKeys:@"application/json",@"Accept",@"application/json; charset=utf-8",@"Content-Type",nil];
+        [self setHeaders:headers];
+    }
+    else
+    {
+        [headers setObject:@"application/json" forKey:@"Accept"];
+        [headers setObject:@"application/json; charset=utf-8" forKey:@"Content-Type"];
+        [self setHeaders:headers];
+    }
+    
+    if (params != nil)
+        [self addQueryStringWithParams:params];
+    
+    switch (httpMethodType)
+    {
+        case kHttpMethodTypeGet:
+        {
+            [httpSessionManager GET:serviceUrl
+                         parameters:params
+                            success:^(NSURLSessionDataTask *task, id responseObject) {
+                                
+                                if (success != nil)
+                                    success(task,responseObject);
+                            }
+                            failure:^(NSURLSessionDataTask *task, NSError *error) {
+                                
+                                if (failure != nil)
+                                    failure(task,error);
+                            }];
+        }
+            break;
+        case kHttpMethodTypePost:
+        {
+            [httpSessionManager POST:serviceUrl
+                          parameters:params
+                             success:^(NSURLSessionDataTask *task, id responseObject) {
+                                 
+                                 if (success != nil)
+                                     success(task,responseObject);
+                             }
+                             failure:^(NSURLSessionDataTask *task, NSError *error) {
+                                 
+                                 if (failure != nil)
+                                     failure(task,error);
+                             }];
+        }
+            break;
+        case kHttpMethodTypeDelete:
+        {
+            [httpSessionManager DELETE:serviceUrl
+                            parameters:params
+                               success:^(NSURLSessionDataTask *task, id responseObject) {
+                                   
+                                   if (success != nil)
+                                       success(task,responseObject);
+                               }
+                               failure:^(NSURLSessionDataTask *task, NSError *error) {
+                                   
+                                   if (failure != nil)
+                                       failure(task,error);
+                               }];
+        }
+            break;
+        case kHttpMethodTypePut:
+        {
+            [httpSessionManager PUT:serviceUrl
+                         parameters:params
+                            success:^(NSURLSessionDataTask *task, id responseObject) {
+                                
+                                if (success != nil)
+                                    success(task,responseObject);
+                            }
+                            failure:^(NSURLSessionDataTask *task, NSError *error) {
+                                
+                                if (failure != nil)
+                                    failure(task,error);
+                            }];
+        }
+            break;
+            
+        default:
+            break;
+    }
+}
+
+- (void) uploadFileRequestWithHttpHeaders:(NSMutableDictionary*) headers
+                          withServiceName:(NSString*) serviceName
+                           withParameters:(NSMutableDictionary*) params
+                             withFileData:(NSArray*) files
+                              withSuccess:(void (^)(NSURLSessionDataTask *task, id responseObject))success
+                              withFailure:(void (^)(NSURLSessionDataTask *task, NSError *error))failure
+{
+    NSAssert(serviceName, ([NSString stringWithFormat:@"\n<------------------->\n%@\n%sn<------------------->",@"serviceName can't be nil",__PRETTY_FUNCTION__]));
+    
+    NSString *serviceUrl = [httpBaseURL stringByAppendingPathComponent:serviceName];
+    
+    if (headers == nil)
+    {
+        NSDictionary *headers = [[NSDictionary alloc] initWithObjectsAndKeys:@"multipart/form-data",@"Content-Type",nil];
+        [self setHeaders:headers];
+    }
+    else
+    {
+        [headers setObject:@"multipart/form-data" forKey:@"Content-Type"];
+        [self setHeaders:headers];
+    }
+    
+    [httpSessionManager POST:serviceUrl
+                  parameters:params
+   constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+       
+       for (NSData *fileData in files)
+       {
+           [formData appendPartWithFileData:fileData name:@"image" fileName:@"future.jpg" mimeType:@"image/jpeg"];
+       }
+   }
+                     success:^(NSURLSessionDataTask *task, id responseObject) {
+                         
+                         if (success != nil)
+                             success(task,responseObject);
+                     }
+                     failure:^(NSURLSessionDataTask *task, NSError *error) {
+                         
+                         if (failure != nil)
+                             failure(task,error);
+                     }];
+}
+
+@end
---
+++ README.md
@@ -0,0 +1,86 @@
+#HTTP-Service
+___
+
+###What is this?
+***
+HTTPService is a class written in Objective-C that wraps functionality of AFHTTPSessionManager for all HTTP communication. It provide two methods, one that uses Raw JSON format and another that uses form-data format as HTTP encoding. You should either config `BASE_URL` in `HTTPService.h` or use designated initializer `- (id)initWithBaseURL:(NSURL*) baseURL andSessionConfig:(NSURLSessionConfiguration*) config`.
+
+###How to use?
+***
+
+HTTPService is based on [**AFNetworking**](http://afnetworking.com). You need to add AFNetworking to your XCode project first - [Github/AFNetworking](https://github.com/AFNetworking/AFNetworking)
+
+After adding AFNetworking, you need to add `HTTPService.h` and `HTTPService.m` to your XCode project. You can initialize HTTPService class object by two ways - 
+
+**1. Default -**
+
+```objective-c
+HTTPService *httpService = [[HTTPService alloc] init];
+```
+
+**Note:** *if you choose default method for initializing then you should config `BASE_URL` in `HTTPService.h`.*
+
+**2. Using designated initializer -**
+
+```objective-c
+NSString *baseURLString = @"http://example.com/mobile/";
+NSURL *baseURL = [NSURL URLWithString:baseURLString];
+NSURLSessionConfiguration *urlSession = [NSURLSessionConfiguration defaultSessionConfiguration];
+HTTPService *httpService = [[HTTPService alloc] initWithBaseURL:baseURL andSessionConfig:urlSession];
+```
+
+Now, you can do HTTP request by using two available instance methods. You can choose one of them based on your HTTP encoding requirement - 
+
+**1. Raw JSON encoding -**
+
+```objective-c
+NSMutableDictionary *headers = [[NSMutableDictionary alloc] initWithObjectsAndKeys:@"ab23tcdsjbvhd",@"token",nil];
+NSMutableDictionary *params = [[NSMutableDictionary alloc] initWithObjectsAndKeys:@"value1",@"param1",@"value2",@"param2", nil];
+[httpService startRequestWithHttpMethod:kHttpMethodTypePost
+                            withHttpHeaders:headers
+                            withServiceName:@"serviceName"
+                             withParameters:params
+                                withSuccess:^(NSURLSessionDataTask *task, id responseObject) {
+                                    NSLog(@"response - %@",responseObject);
+                                }
+                                withFailure:^(NSURLSessionDataTask *task, NSError *error) {
+                                    NSLog(@"error - %@",[error localizedDescription]);
+                                }];
+```
+
+#####You need to pass appropriate HTTP Method to it from following - 
+
+`GET` - *kHttpMethodTypeGet*
+
+`POST` - *kHttpMethodTypePost*
+
+`DELETE` - *kHttpMethodTypeDelete*
+
+`PUT` - *kHttpMethodTypePut*
+
+
+**Note:** *There is no need to pass **Content-Type** and **Accept** in header.*
+
+**2. Multipart form data -**
+
+```objective-c
+NSMutableDictionary *headers = [[NSMutableDictionary alloc] initWithObjectsAndKeys:@"ab23tcdsjbvhd",@"token",nil];
+NSMutableDictionary *params = [[NSMutableDictionary alloc] initWithObjectsAndKeys:@"value1",@"param1",@"value2",@"param2", nil];
+NSData *fileData = UIImageJPEGRepresentation(image, 1.0);
+[httpService uploadFileRequestWithHttpHeaders:headers
+                                  withServiceName:@"serviceName"
+                                   withParameters:params
+                                     withFileData:[NSArray arrayWithObject:fileData]
+                                      withSuccess:^(NSURLSessionDataTask *task, id responseObject) {
+                                          NSLog(@"response - %@",responseObject);
+                                      }
+                                      withFailure:^(NSURLSessionDataTask *task, NSError *error) {
+                                          NSLog(@"error - %@",[error localizedDescription]);
+                                      }];
+```
+
+**Note:** 
+
+* *There is no need to pass **Content-Type** in header.*
+
+* *This methods takes array of NSData objects, so you can pass multiple files in it.*
